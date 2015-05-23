6c16d7f6cb884523e1354fe2f8fc2f79ad360940
hung-phan <colorvisavn@gmail.com>
2015-05-24 02:57:21 +0700

    update emacs

--- config/setup-flycheck.el
+++ config/setup-flycheck.el
@@ -6,8 +6,8 @@
 ;; -----------------------------------------------------------------------------
 (defun colorvisa/adjust-flycheck-automatic-syntax-eagerness ()
   "Adjust how often we check for errors based on if there are any.
-This lets us fix any errors as quickly as possible, but in a
-clean buffer we're an order of magnitude laxer about checking."
+  This lets us fix any errors as quickly as possible, but in a
+  clean buffer we're an order of magnitude laxer about checking."
   (setq flycheck-idle-change-delay
         (if flycheck-current-errors 0.5 30.0)))
 
@@ -34,12 +34,14 @@ clean buffer we're an order of magnitude laxer about checking."
 
 (defun flycheck-handle-idle-change ()
   "Handle an expired idle time since the last change.
-This is an overwritten version of the original
-flycheck-handle-idle-change, which removes the forced deferred.
-Timers should only trigger inbetween commands in a single
-threaded system and the forced deferred makes errors never show
-up before you execute another command."
+  This is an overwritten version of the original
+  flycheck-handle-idle-change, which removes the forced deferred.
+  Timers should only trigger inbetween commands in a single
+  threaded system and the forced deferred makes errors never show
+  up before you execute another command."
   (flycheck-clear-idle-change-timer)
   (flycheck-buffer-automatically 'idle-change))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide 'setup-flycheck)
