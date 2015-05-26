536a6604c25418306527b43c6f14cfdc96fabcd2
hung-phan <colorvisavn@gmail.com>
2015-05-26 20:47:29 +0700

    update emacs webmode

--- config/setup-web-mode.el
+++ config/setup-web-mode.el
@@ -8,8 +8,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist
-'("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
 ;; make web-mode play nice with smartparens
 (setq web-mode-enable-auto-pairing nil)
