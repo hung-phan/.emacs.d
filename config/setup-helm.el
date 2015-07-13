e84e21400cb513f76e7cb21828cda56f79371c36
hung-phan <colorvisavn@gmail.com>
2015-07-13 23:18:28 +0700

    update emacs

--- config/setup-helm.el
+++ config/setup-helm.el
@@ -5,12 +5,23 @@
 (require 'helm-projectile)
 (require 'helm-flycheck)
 
-(projectile-global-mode)
-(setq projectile-enable-caching t)
-
-(helm-mode 1)
-
 (colorvisa/in '(darwin)
   (setq helm-locate-command "mdfind %s %s"))
+
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+(helm-mode 1)
+(helm-descbinds-install)
+
+(setq
+ helm-mp-highlight-delay 0.7
+ helm-mp-highlight-threshold 4
+ helm-maybe-use-default-as-input nil
+ helm-quick-update t
+ helm-idle-delay 0.01
+ helm-input-idle-delay 0.01
+ helm-always-two-windows nil
+ helm-split-window-default-side 'other
+ helm-candidate-number-limit 200)
 
 (provide 'setup-helm)
