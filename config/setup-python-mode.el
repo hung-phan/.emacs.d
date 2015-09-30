b48d0d0460e280a9f086601a670ca942e8731a80
hung-phan <colorvisavn@gmail.com>
2015-09-30 01:18:26 -0700

    Update emacs

--- config/setup-python-mode.el
+++ config/setup-python-mode.el
@@ -1,5 +1,7 @@
-(package-initialize)
-(setq elpy-rpc-backend "jedi")
-(elpy-enable)
+(colorvisa/set-up 'elpy
+  (package-initialize)
+  (setq elpy-modules (delq 'elpy-module-company elpy-modules))
+  (setq elpy-rpc-backend "jedi")
+  (elpy-enable))
 
 (provide 'setup-python-mode)
