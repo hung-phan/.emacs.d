6e7a23b4173710b590da0a664f6d43341e80afb9
hung-phan <colorvisavn@gmail.com>
2016-01-07 23:00:08 +0700

    Update mappings

--- config/setup-mappings.el
+++ config/setup-mappings.el
@@ -1,11 +1,14 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen)
 
-(define-key global-map (kbd "<f2>")  'which-key-show-top-level)
-(define-key global-map (kbd "<f5>")  'escreen-create-screen)
-(define-key global-map (kbd "<f6>")  'escreen-kill-screen)
-(define-key global-map (kbd "<f7>")  'escreen-goto-prev-screen)
-(define-key global-map (kbd "<f8>")  'escreen-goto-next-screen)
+(colorvisa/set-up 'which-key
+  (define-key global-map (kbd "<f2>")  'which-key-show-top-level))
+
+(colorvisa/set-up 'escreen
+  (define-key global-map (kbd "<f5>")  'escreen-create-screen)
+  (define-key global-map (kbd "<f6>")  'escreen-kill-screen)
+  (define-key global-map (kbd "<f7>")  'escreen-goto-prev-screen)
+  (define-key global-map (kbd "<f8>")  'escreen-goto-next-screen))
 
 ;; Font size
 (global-set-key (kbd "C-+") 'text-scale-increase)
