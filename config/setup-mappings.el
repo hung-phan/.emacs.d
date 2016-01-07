d0255a63f5884f6ae48ffff44fc2a241382cc383
hung-phan <colorvisavn@gmail.com>
2016-01-07 12:28:46 +0700

    Update emacs

--- config/setup-mappings.el
+++ config/setup-mappings.el
@@ -1,6 +1,7 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (define-key global-map (kbd "<s-return>") 'toggle-frame-fullscreen)
 
+(define-key global-map (kbd "<f2>")  'which-key-show-top-level)
 (define-key global-map (kbd "<f5>")  'escreen-create-screen)
 (define-key global-map (kbd "<f6>")  'escreen-kill-screen)
 (define-key global-map (kbd "<f7>")  'escreen-goto-prev-screen)
